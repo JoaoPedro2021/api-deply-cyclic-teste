0fdd227d3b7730b92819396cc94b7d7ee45ee4a9
JoaoPedro2021 <jpss7693@gmail.com>
2023-03-15 11:27:57 -0300

    return initial project

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,8 +7,11 @@ const port = process.env.DB_PORT as number | undefined
 
 export const AppDataSource = new DataSource({
     type: "postgres",
+    host: process.env.DB_HOST,
     port: port,
-    url: process.env.DB_URI,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     entities: [`${__dirname}/**/entities/*.{ts,js}`],
     migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 
